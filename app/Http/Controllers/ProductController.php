923e503d2b5807b59632cde80227db02be2e3674
Prashant <prashantbasnet222@gmail.com>
2024-07-05 22:17:45 +0530

    worked on update & delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,15 +7,15 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    // insert product function
     public function store(Request $request)
     {
         $request->validate([
-            'name' =>'required|max:191',
-            'description'=>'required|max:191',
-            'price'=>'required|max:191',
-            'qty'=>'required|max:191'
+            'name' => 'required|max:191',
+            'description' => 'required|max:191',
+            'price' => 'required|max:191',
+            'qty' => 'required|max:191',
         ]);
-
 
         $product = new Product;
         $product->name = $request->name;
@@ -26,18 +26,61 @@ class ProductController extends Controller
         return response()->json(['message' => 'Product added successfully'], 200);
     }
 
-    public function index(){
+    // show all products function
+    public function index()
+    {
         $products = Product::all();
-        return response()->json(['products' =>$products] , 200);
+        return response()->json(['products' => $products], 200);
 
     }
-    public function show($id){
+
+    // show only one product function
+    public function show($id)
+    {
         $product = Product::find($id);
         if ($product) {
-            return response()->json(['product' =>$product] , 200);
+            return response()->json(['product' => $product], 200);
         } else {
-            return response()->json(['message' =>'No Product Found !'] , 404);
+            return response()->json(['message' => 'No Product Found !'], 404);
         }
 
+    }
+
+
+    // update product function
+    public function update(Request $request, $id)
+    {
+
+        $request->validate([
+            'name' => 'required|max:191',
+            'description' => 'required|max:191',
+            'price' => 'required|max:191',
+            'qty' => 'required|max:191',
+        ]);
+
+        $product = Product::find($id);
+        if ($product) {
+            $product->name = $request->name;
+            $product->description = $request->description;
+            $product->price = $request->price;
+            $product->qty = $request->qty;
+            $product->update();
+            return response()->json(['message' => 'Product updated successfully'], 200);
+        } else {
+            return response()->json(['message' => 'No Product Found !'], 404);
+        }
+
+    }
+
+    // delete product function
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        if ($product) {
+            $product->delete();
+            return response()->json(['message' => ' Product Deleted Successfully'], 200);
+        } else {
+            return response()->json(['message' => 'No Product Found'], 404);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// api for insert product
 Route::post('product/add',[ProductController::class,'store']);
+// api for showing all the products
 Route::get('products',[ProductController::class,'index']);
+// api for showing only one  product
 Route::get('product/{id}/show',[ProductController::class,'show']);
+
+// api for update product
+//for raw data
+Route::put('product/{id}/update',[ProductController::class,'update']);
+//for form body
+// Route::post('product/{id}/update',[ProductController::class,'update']);
+
+
+// api for update product
+Route::delete('product/{id}/delete',[ProductController::class,'destroy']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
